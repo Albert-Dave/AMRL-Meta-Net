5c48e7f548ed454587f9756ce33410c235c955d6
all4dave <113103428+Albert-Dave@users.noreply.github.com>
2025-12-25 23:56:18 +0800

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,29 @@
+import csv
+import os
+from datetime import datetime
 
+class Logger:
+    def __init__(self, log_dir="logs", filename=None):
+        os.makedirs(log_dir, exist_ok=True)
+
+        if filename is None:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f"log_{timestamp}.csv"
+
+        self.path = os.path.join(log_dir, filename)
+        self.file = open(self.path, "w", newline="")
+        self.writer = None
+
+    def write(self, data: dict):
+        """
+        data: dictionary of scalar values
+        """
+        if self.writer is None:
+            self.writer = csv.DictWriter(self.file, fieldnames=data.keys())
+            self.writer.writeheader()
+
+        self.writer.writerow(data)
+        self.file.flush()
+
+    def close(self):
+        self.file.close()
